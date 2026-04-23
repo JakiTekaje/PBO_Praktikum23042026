fe366833df3ccb83cac377fed7f425c52f1d7c51
JakiTekaje <zaky.ahmad849@gmail.com>
2026-04-23 19:16:48 +0700

    Add files via upload

---
+++ MainAuthor.java
@@ -0,0 +1,18 @@
+/*
+Nama    : Ahmad Zaky, NIM  : 2501082008
+Desk    : 
+*/
+package praktikum23042026;
+
+public class MainAuthor {
+
+    public static void main(String[] args) {
+        Author Rin = new Author ("Rinaldi Munir","rinaldi@gmail.com", 'm');
+        System.out.println(Rin);
+        
+        Rin.setEmail("rinaldi@gmail.com");
+        System.out.println("Nama : "+Rin.getNama());
+        System.out.println("Gender : "+Rin.getGender());
+        System.out.println("Email : "+Rin.getEmail());
+    }
+}
---
+++ MainBook.java
@@ -0,0 +1,29 @@
+/*
+Nama    : Ahmad Zaky, NIM  : 2501082008
+Desk    : 
+*/
+package praktikum23042026;
+
+public class MainBook {
+
+    public static void main(String[] args) {
+        Author Rin = new Author("Rinaldi Munir","rinaldi@gmail.com",'m');
+        System.out.print(Rin);
+        
+        Book dummybook = new Book("Algoritma dan Pemograman",Rin,98000,99);
+        System.out.println(dummybook);
+        dummybook.setPrice(120000);
+        dummybook.setQty(88);
+        System.out.println("Nama : "+dummybook.getNama());
+        System.out.println("Price : "+dummybook.getPrice());
+        System.out.println("Qty : "+dummybook.getQty());
+        System.out.println("Author : "+dummybook.getAuthor());
+        System.out.println("Author name : "+dummybook.getAuthor().getNama());
+        System.out.println("Author email : "+dummybook.getAuthor().getEmail());
+        System.out.println("Authir gender : "+dummybook.getAuthor().getGender());
+        
+        Book newDummybook = new Book("Java for Dummies", new Author("Peter Lee","peter@gmail.com",'m'),19.99,8);
+        System.out.println(newDummybook);
+    }
+    
+}
